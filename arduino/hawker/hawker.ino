b8d7df05a32d1434bbf90ad6f26ab4c1f97c547e
Ayush K Pacheriwala <ayushpacheriwala@gmail.com>
2015-01-14 19:23:02 +0800

    initial commit

---
+++ arduino/hawker/hawker.ino
@@ -0,0 +1,68 @@
+#include <Keypad.h>
+#include <SoftwareSerial.h>
+
+// Attach the serial display's RX line to digital pin 2
+SoftwareSerial mySerial(2,3); // pin 3 = TX, pin 2 = RX (unused)
+void LCDWrite(char input);
+
+const byte ROWS = 4; // Four rows
+const byte COLS = 3; // Three columns
+// Define the Keymap
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'#','0','*'}
+};
+// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte rowPins[ROWS] = { 9, 8, 7, 6 };
+// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+byte colPins[COLS] = { 12, 11, 10 }; 
+
+// Create the Keypad
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+#define ledpin 13
+
+void setup()
+{
+  mySerial.begin(9600); // set up serial port for 9600 baud
+  delay(500); // wait for display to boot up
+  pinMode(ledpin,OUTPUT);
+  digitalWrite(ledpin, HIGH);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char key = kpd.getKey();
+  if(key)  // Check for a valid key.
+  {
+    switch (key)
+    {
+      case '*':
+        digitalWrite(ledpin, LOW);
+        break;
+      case '#':
+        digitalWrite(ledpin, HIGH);
+        break;
+      default:
+        Serial.println(key);
+    }
+  }
+  LCDWrite(key);
+}
+
+void LCDWrite(char input)
+{
+  mySerial.write(254); // move cursor to beginning of first line
+  mySerial.write(128);
+
+  mySerial.write("                "); // clear display
+  mySerial.write("                ");
+
+  mySerial.write(254); // move cursor to beginning of first line
+  mySerial.write(128);
+ 
+  mySerial.write(input);
+}
